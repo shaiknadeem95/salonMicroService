be8d4b053b08146da10b0bb158798557960196aa
shaiknadeem95 <shaiknadeem98@gmail.com>
2025-07-12 18:52:48 +0530

    adding the .env file and removing the secreate files

--- salonUserService/src/main/java/com/salaon/userservice/SalonUserServiceApplication.java
+++ salonUserService/src/main/java/com/salaon/userservice/SalonUserServiceApplication.java
@@ -1,5 +1,6 @@
 package com.salaon.userservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SalonUserServiceApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+
 		SpringApplication.run(SalonUserServiceApplication.class, args);
 	}
 
